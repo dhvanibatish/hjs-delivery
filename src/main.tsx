9e041498aa620426e1efe8b0b9b07d7648ad0571
dhvanibatish <dhvanibatish16@gmail.com>
2026-07-21 12:09:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Pickups from './Pickups.tsx'
+
+const isPickup = new URLSearchParams(window.location.search).has('pickups')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isPickup ? <Pickups /> : <App />}
   </StrictMode>,
 )
