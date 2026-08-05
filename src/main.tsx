4804376fd2ce8a88e174012c1863ffc97d8e0cbc
dhvanibatish <dhvanibatish16@gmail.com>
2026-08-05 16:34:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import Pickups from './Pickups.tsx'
 import ComplaintApp from './ComplaintApp.tsx'
+import Attendance from './Attendance.tsx'
 
 const params = new URLSearchParams(window.location.search)
 const isPickup = params.has('pickups')
 const isComplaint = params.has('complaints')
+const isAttendance = params.has('attendance')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isComplaint ? <ComplaintApp /> : isPickup ? <Pickups /> : <App />}
+    {isComplaint ? <ComplaintApp /> : isPickup ? <Pickups /> : isAttendance ? <Attendance /> : <App />}
   </StrictMode>,
 )
